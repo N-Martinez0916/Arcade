32ea5d933202fae9910c14e5d6acc56a80d1caa2
N-Martinez0916 <nicomartin0916@gmail.com>
2022-10-06 17:56:24 -0400

    tic tac toe

--- app.js
+++ app.js
@@ -1,28 +1,64 @@
 // state
-let state;
-const board = [
-    4, 4, 4, 4, 4, 4, 0, /* player 1 */ 4, 4, 4, 4, 4, 4, 0 /* player 2 */
-  ];
-  const gameState = {
-    board: board, // from above
-    currentPlayer: 0 // switch to 1 when the player swaps
-  }
-function buildInitialState() {
+let state = document.getElementById('board');
+let bot = document.getElementById('')
 
+let player1 = document.getElementById('player1')
+let name1 = document.getElementById('name1')
+// const text1 = document.getElementById('text1')
+const submit1 = document.getElementById('submit1')
+const score1 = document.getElementById('score1')
+
+let player2 = document.getElementById('player2')
+let name2 = document.getElementById('name2')
+// const text2 = document.getElementById('text2')
+const submit2 = document.getElementById('submit2')
+const score2 = document.getElementById('score2')
+
+
+
+
+
+
+const gameState = {
+    players: ['X', 'O'],
+    board: [
+      [null, null, null],
+      [null, null, null],
+      [null, null, null]
+    ]
 }
 
-// render
-function renderState() {
+// let boardStatus = gameState.board[[[]]]
+let currentPlayer = gameState.players[0]
+state.addEventListener ('click', function (event){
+    if (event.target === null) {
+        event.target.innerHTML = currentPlayer
+        console.log(boardStatus)
+        currentPlayer = gameState.players[1]
+        }
+gameState.players.reverse()
+})
 
-}
+// function buildInitialState() {
+// }
 
-// maybe a dozen or so helper functions for tiny pieces of the interface
+// // render
+// function renderState() {
 
-// listeners
-function onBoardClick() {
-  // update state, maybe with another dozen or so helper functions...
+// }
 
-  renderState() // show the user the new state
-}
-const board = document.getElementById('board');
-board.addEventListener('click', onBoardClick); // etc
+// // maybe a dozen or so helper functions for tiny pieces of the interface
+
+// // listeners
+// function onBoardClick("click" function() {
+
+
+
+
+// }) {
+//   // update state, maybe with another dozen or so helper functions...
+
+//   renderState() // show the user the new state
+// }
+// const board = document.getElementById('board');
+// board.addEventListener('click', onBoardClick); // etc
