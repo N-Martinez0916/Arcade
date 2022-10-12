dafc31ed738ae21172b22d78ae85b0c458d302dd
N-Martinez0916 <nicomartin0916@gmail.com>
2022-10-12 00:23:21 -0400

    Arcade 10/12

--- app.js
+++ app.js
@@ -1,64 +1,47 @@
-// state
-let state = document.getElementById('board');
-let bot = document.getElementById('')
+let board = document.querySelectorAll('table td')
+const winMessage = document.getElementById('winMessage')
+function isWin(board) {
+  let winCombos = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ]
 
-let player1 = document.getElementById('player1')
-let name1 = document.getElementById('name1')
-// const text1 = document.getElementById('text1')
-const submit1 = document.getElementById('submit1')
-const score1 = document.getElementById('score1')
+  for (let combo of winCombos) {
+    if (
+      board[combo[0]].textContent == board[combo[1]].textContent && board[combo[1]].textContent == board[combo[2]].textContent && board[combo[0]].textContent != ''
+    ) {
+      return true
+    }
+  }
+    return false
 
-let player2 = document.getElementById('player2')
-let name2 = document.getElementById('name2')
-// const text2 = document.getElementById('text2')
-const submit2 = document.getElementById('submit2')
-const score2 = document.getElementById('score2')
-
-
-
-
-
-
-const gameState = {
-    players: ['X', 'O'],
-    board: [
-      [null, null, null],
-      [null, null, null],
-      [null, null, null]
-    ]
 }
 
-// let boardStatus = gameState.board[[[]]]
-let currentPlayer = gameState.players[0]
-state.addEventListener ('click', function (event){
-    if (event.target === null) {
-        event.target.innerHTML = currentPlayer
-        console.log(boardStatus)
-        currentPlayer = gameState.players[1]
-        }
-gameState.players.reverse()
-})
+startGame(board)
 
-// function buildInitialState() {
-// }
+function startGame(board) {
+  let i = 0
+  for (let box of board) {
+    box.addEventListener('click', function playerMoves() {
+      if (i % 2 === 0) {
+      this.textContent = 'X'
+      } else {
+        this.textContent = 'O'
+      }
 
-// // render
-// function renderState() {
-
-// }
-
-// // maybe a dozen or so helper functions for tiny pieces of the interface
-
-// // listeners
-// function onBoardClick("click" function() {
-
-
-
-
-// }) {
-//   // update state, maybe with another dozen or so helper functions...
-
-//   renderState() // show the user the new state
-// }
-// const board = document.getElementById('board');
-// board.addEventListener('click', onBoardClick); // etc
+      if (isWin(board)) {
+        winMessage.innerHTML = `Player ${this.textContent} Wins!!`
+        
+      } else if (i == 8) {
+        winMessage.innerHTML = `DRAW`
+      }
+      i++
+    })
+  }
+}
